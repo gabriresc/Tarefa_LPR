0e962351d4f7150b29cbffdbcb907203bd6b6340
gabriresc <gabriresc@gmail.com>
2025-03-20 14:07:50 -0300

    3

--- exercicio03csharp/Program.cs
+++ exercicio03csharp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int classe;
+
+Console.WriteLine("Escolha sua classe:1-Guerreiro\n2-Mago\n3-Arqueira");
+classe = int.Parse(Console.ReadLine());
+
+switch(classe){
+    case 1:
+        Console.WriteLine("Habilidades:Ataque Pesado, Defesa Total");
+        break;
+    case 2:
+        Console.WriteLine("Habilidades:Bola de Fogo, Escudo de Gelo");
+        break;
+    case 3:
+        Console.WriteLine("Habilidades:Flecha Precisa, Disparo Triplo");
+        break;
+
+}
+
