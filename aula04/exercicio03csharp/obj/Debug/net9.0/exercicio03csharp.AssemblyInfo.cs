dd36550ccda065f48f0aa880340782b1551442f5
gabriresc <gabriresc@gmail.com>
2025-03-20 14:11:49 -0300

    aula04

--- aula04/exercicio03csharp/obj/Debug/net9.0/exercicio03csharp.AssemblyInfo.cs
+++ aula04/exercicio03csharp/obj/Debug/net9.0/exercicio03csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercicio03csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66775dc549b0851b053ad5fa9127a7b2254dcf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e962351d4f7150b29cbffdbcb907203bd6b6340")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercicio03csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercicio03csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
